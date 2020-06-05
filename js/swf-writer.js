1a141fc6079039eb3d11636fcbcb04140447b05d
NSSVelocity <jonathanjwhite@hotmail.com>
2020-06-05 07:12:16 -0230

    Remove unused function in swf-writer.

--- js/swf-writer.js
+++ js/swf-writer.js
@@ -1,10 +1,3 @@
-function concatBuffer(buffer1, buffer2) {
-	var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-	tmp.set(new Uint8Array(buffer1), 0);
-	tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-	return tmp.buffer;
-};
-
 function ByteWriter(rawSize) {
 	this.baseSize = (rawSize == null || isNaN(rawSize)) ? 1024 : rawSize;
 	this.textEncoder = new TextEncoder();
